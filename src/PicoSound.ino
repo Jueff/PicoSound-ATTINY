f2b5f420dcf8a1c78ca4958a540df94abe7fd41f
Jueff <Jueff@a1.net>
2026-03-15 17:13:35 +0100

    move sound classes to seperate library

--- src/PicoSound.ino
+++ src/PicoSound.ino
@@ -1,9 +1,9 @@
 /*
  * SPDX-FileCopyrightText: 2025-2026 Juergen Winkler <MobaLedLib@gmx.at>
- * SPDX-License-Identifier: CC-BY-NC-4.0
+ * SPDX-License-Identifier: BSL-1.1
  *
  * Description:
- *  This firmware runs on a Raspberry Pi Pico and control up to 6 sound modules
+ *  This firmware runs on a Raspberry Pi Pico and controls up to 6 sound modules
  *  It is part of the MobaLedLib project. It receives LED data in groups of three byte values
  *  parses the data and controls the JQ6500 or MP3-TF16P sound modules via serial line.
  *  It provides visual feedback via the onboard RGB LED.
